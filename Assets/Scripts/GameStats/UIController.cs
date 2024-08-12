e99f6a19690a64cf7d4cab62f0d29f1ec23e2fb3
fmauz <fmauz@BASDA-5663.local>
2024-08-12 21:43:24 +0200

    feat: add possibility to toggle player empire group

---
+++ Assets/Scripts/GameStats/UIController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+
+namespace GameStats{
+    public class UIController : MonoBehaviour{
+        [SerializeField]
+        private GameObject player_empires_classic;
+        [SerializeField]
+        private GameObject player_empires_legends;
+
+        private UI_STATE playerState;
+        private UI_STATE automaState;
+        [SerializeField]
+        private TextMeshProUGUI playerButtonText;
+        [SerializeField]
+        private TextMeshProUGUI automaButtonText;
+
+        void Start(){
+           playerState=UI_STATE.CLASSIC;
+           automaState=UI_STATE.CLASSIC;
+           UpdateUI();
+        }
+
+        public void clickPlayerButton(){
+            if(playerState==UI_STATE.CLASSIC){
+                playerState=UI_STATE.LEGENDS;
+            }else{
+                playerState=UI_STATE.CLASSIC;
+            }
+            UpdateUI();
+        }
+
+        public void UpdateUI(){
+            player_empires_classic.SetActive(playerState==UI_STATE.CLASSIC);
+            player_empires_legends.SetActive(playerState==UI_STATE.LEGENDS);
+
+            if(playerState==UI_STATE.CLASSIC){
+                playerButtonText.text="Classic";
+            }else{
+                playerButtonText.text="Legends";
+            }
+        }
+
+    }
+
+    public enum UI_STATE{
+        CLASSIC,
+        LEGENDS,
+        HORIZONTS
+    }
+
+   
+}
