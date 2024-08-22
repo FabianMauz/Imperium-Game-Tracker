47bbe4b4db8fcb1a6c89f9954087f3fc7f0841e6
Fabian Mauz <fabian66@freenet.de>
2024-08-22 14:35:51 +0200

    add possibility to make transitions smoother (#16)
    - make transition smoother. Solves https://github.com/FabianMauz/Imperium-Game-Tracker/issues/14

---
+++ Assets/Scripts/Tools/Appear.cs
@@ -0,0 +1,109 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Appear : MonoBehaviour
+{
+    [SerializeField]
+    private Image[] images;
+
+    private TextMeshProUGUI[] texts;
+
+    [SerializeField]
+    private bool increaseAlpha;
+
+    [SerializeField]
+    private bool decreaseAlpha;
+
+    [SerializeField]
+    private float increaseSpeed = 10;
+
+    [SerializeField]
+    private float decreaseSpeed = 10;
+
+    public void startAppearing()
+    {
+        this.gameObject.SetActive(true);
+        this.increaseAlpha = true;
+        this.decreaseAlpha = false;
+    }
+
+    public void startDisappearing()
+    {
+        this.increaseAlpha = false;
+        this.decreaseAlpha = true;
+    }
+
+    void Start()
+    {
+        texts = GetComponentsInChildren<TextMeshProUGUI>();
+        images = GetComponentsInChildren<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (TextMeshProUGUI text in texts)
+        {
+            handleText(text);
+        }
+        foreach (Image image in images)
+        {
+            handleImage(image);
+        }
+    }
+
+    private void handleText(TextMeshProUGUI text)
+    {
+        if (increaseAlpha)
+        {
+            if (text.alpha < 1)
+            {
+                text.alpha += increaseSpeed * Time.deltaTime;
+            }
+        }
+        if (decreaseAlpha)
+        {
+            if (text.alpha > 0)
+            {
+                text.alpha -= decreaseSpeed * Time.deltaTime;
+            }
+            if (text.alpha <= 0)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void handleImage(Image image)
+    {
+        if (increaseAlpha)
+        {
+            Color c = image.color;
+            if (c.a < .3f)
+            {
+                c.a = 0.3f;
+            }
+            if (c.a < 1)
+            {
+                c.a += increaseSpeed * Time.deltaTime;
+                if (c.a > 1)
+                {
+                    c.a = 1;
+                }
+                //c.a=Math.Clamp(c.a,c.a,1);
+                image.color = c;
+            }
+        }
+        if (decreaseAlpha)
+        {
+            Color c = image.color;
+            c.a -= decreaseSpeed * Time.deltaTime;
+            image.color = c;
+            if (c.a <= 0)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+}
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -10,17 +10,20 @@ public class UIController : MonoBehaviour
     [SerializeField]
     private GameObject resultOverview;
     private MAIN_UI_STATE currentState = MAIN_UI_STATE.MAIN_MENU;
+    private Dictionary<MAIN_UI_STATE, GameObject> mainObjectsMap =
+        new Dictionary<MAIN_UI_STATE, GameObject>();
+
+    private void Start()
+    {
+        mainObjectsMap.Add(MAIN_UI_STATE.MAIN_MENU, mainMenu);
+        mainObjectsMap.Add(MAIN_UI_STATE.RESULT_OVERVIEW, resultOverview);
+    }
 
     public void switchToMenu(string newState)
     {
-        this.currentState =  (MAIN_UI_STATE)System.Enum.Parse(typeof(MAIN_UI_STATE), newState);
-        updateUI();
-    }
-
-    private void updateUI()
-    {
-        mainMenu.SetActive(currentState == MAIN_UI_STATE.MAIN_MENU);
-        resultOverview.SetActive(currentState == MAIN_UI_STATE.RESULT_OVERVIEW);
+        mainObjectsMap[currentState].GetComponent<Appear>().startDisappearing();
+        this.currentState = (MAIN_UI_STATE)System.Enum.Parse(typeof(MAIN_UI_STATE), newState);
+        mainObjectsMap[currentState].GetComponent<Appear>().startAppearing();
     }
 
     public enum MAIN_UI_STATE
