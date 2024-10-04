ba26c44d46e24bd6e6822cae0a7107451f751c15
FabianMauz <fabian66@freenet.de>
2024-10-04 22:10:42 +0200

    add difficulty options

---
+++ Assets/Scripts/Domain/Difficulty.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Domain
+{
+    [Serializable]
+    public enum Difficulty
+    {
+        CHIEFTAIN,
+        WARLORD,
+        IMPERATOR,
+        SOVEREIGN,
+        OVERLORD
+    }
+}
--- Assets/Scripts/Scoring/Scoring.cs
+++ Assets/Scripts/Scoring/Scoring.cs
@@ -1,17 +1,30 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using Domain;
 using TMPro;
 using UnityEngine;
 
-public class Scoring : MonoBehaviour{
-    private string pointsFromPlayer="0";
-    private string pointsFromAutoma="0";
+public class Scoring : MonoBehaviour
+{
+    private string pointsFromPlayer = "0";
+    private string pointsFromAutoma = "0";
+
+    private Difficulty difficulty;
 
     [SerializeField]
     private TextMeshProUGUI inputPlayer;
-   public void saveGameResult(){
-print(":"+inputPlayer.text);
-   }
-   
+
+    public void saveGameResult()
+    {
+        print("save game:" + inputPlayer.text);
+    }
+
+    public void setDifficulty(string difficulty)
+    {
+        this.difficulty = (Difficulty)Enum.Parse(typeof(Difficulty), difficulty);
+        updateUI();
+    }
+
+    private void updateUI(){
+print("new difficulty: "+difficulty);
+    }
 }
